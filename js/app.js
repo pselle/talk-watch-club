eaf6583666112d0bc736ce5bc8e22babe5ea0fba
pselle <pamela.selle@gmail.com>
2018-06-27 21:26:23 -0400

    Previous vids and query param support

--- js/app.js
+++ js/app.js
@@ -29,8 +29,19 @@ function createVideoElement(talk) {
   return clone;
 }
 
+function populatePrevious(list, idx, talks) {
+  for(var i = idx-1; i >= 0; i--) {
+    const t = document.getElementById('talkItem');
+    const clone = document.importNode(t.content, true);
+    const link = clone.querySelector('a');
+    link.href = `?video=${i}`;
+    link.textContent = talks[i].title;
+    list.appendChild(clone);
+  }
+}
 const talks = [
   {
+    title: "Aditya",
     url: "https://vimeo.com/221049715",
     blurb: "Aditya talks at Monitorama"
   },
@@ -46,11 +57,25 @@ const week0 = new Date('June 18, 2018 00:00:00');
 // Get the index for the week
 const idx = moment().diff(week0, 'week');
 
-const talk = talks[idx];
+var talk = talks[idx];
+const queryParamVid = getParameterByName('video', window.location);
+if (queryParamVid) {
+  talk = talks[Number(queryParamVid)];
+}
 
 document.getElementById('video').appendChild(createVideoElement(talk));
 
-// Display a list of past talks and what's up next week,
+// Print previous videos
+const list = document.querySelector('#past ol');
+populatePrevious(list, idx, talks);
+
+// What's up next week?
+if (idx + 1 < talks.length) {
+  console.log('something next week')
+} else {
+  console.log('nothing next week! make a PR!')
+}
+
 // If there's nothing up next week instruct people to make a PR
 // TODO make it easy to make a PR
 
